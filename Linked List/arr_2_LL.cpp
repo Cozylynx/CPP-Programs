fdecb6782ba539f65c43db510652837661a82384
Cozylynx <thapagaurav.tg777@gmail.com>
2024-03-12 01:15:14 +0530

    edits

--- Linked List/arr_2_LL.cpp
+++ Linked List/arr_2_LL.cpp
@@ -17,12 +17,12 @@ class Node{
     }
 };
  Node* ConvertArray2LL(vector<int> &arr){
-        Node* head = new Node(arr[0]);
-        Node* mover = head;
+        Node* head = new Node(arr[0]);  // make a new pointer head which stores the address of the first index.
+        Node* mover = head; // a mover which helps in linking the LL.
         for(int i =0;i<arr.size();i++){
-            Node* temp = new Node (arr[i]);
-            mover->next = temp;
-            mover = temp;
+            Node* temp = new Node (arr[i]); //creates a temporary new node which will have the value of the arr at ith index.
+            mover->next = temp; // giving the address of next node to the mover at ith index.
+            mover = temp;  // moving the mover to the next  position.
         }
         return head;
     }
@@ -30,7 +30,7 @@ class Node{
 int main(){
    
     vector<int> arr = {2,5,8,7};
-    Node* head = ConvertArray2LL(arr);
+    Node* head = ConvertArray2LL(arr);  // it will return the head which is 2; and the arr is converted now.    
     cout<<head->data;
    
 }
